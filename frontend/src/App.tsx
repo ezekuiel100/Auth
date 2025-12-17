e143902c9d876de2e85c0d8c4406a5f725edb8df
unknown <ezekuiel100@gmail.com>
2025-12-17 13:44:48 -0300

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="login">
+      <h1>Faça login</h1>
+      <input type="text" placeholder="Email" />
+      <input type="password" placeholder="Senha" />
+      <button>Entrar</button>
+
+      <a>Criar conta</a>
+    </div>
+  );
+}
+
+export default App;
