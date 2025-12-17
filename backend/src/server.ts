b5e1a5323dea5afae21ba8da839d4ecbb4bb74f6
unknown <ezekuiel100@gmail.com>
2025-12-17 16:32:41 -0300

    server

---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Hello TypeScript!");
+});
+
+server.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000");
+});
