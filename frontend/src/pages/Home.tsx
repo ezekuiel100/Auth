59d2e705b37e134c9e15cefb8bf2dc1277c714cb
unknown <ezekuiel100@gmail.com>
2025-12-22 11:58:23 -0300

    update user info

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useUser } from "../AuthProvider";
 import { useNavigate } from "react-router";
 
@@ -6,19 +6,48 @@ export default function Home() {
   const { user, signOut, updateUser } = useUser();
   const navigate = useNavigate();
 
-  const [form, setForm] = useState(user || { name: "", email: "" });
+  const [form, setForm] = useState({ name: "", email: "" });
   const [isEditing, setIsEditing] = useState(false);
+
+  useEffect(() => {
+    if (user) {
+      setForm(user);
+    }
+  }, [user]);
 
   if (!user) {
     return <p>Carregando ou usuário não autenticado...</p>;
   }
 
-  function handleUpdate(e: React.FormEvent) {
+  async function handleUpdate(e: React.FormEvent) {
     e.preventDefault();
+    try {
+      const res = await fetch("http://localhost:3000/user/update", {
+        method: "PUT",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
 
-    updateUser(form);
-    setIsEditing(false);
-    alert("Perfil atualizado com sucesso!");
+      if (res.status === 401) {
+        alert("Sua sessão expirou!");
+        navigate("/login");
+        return;
+      }
+
+      if (!res.ok) return;
+
+      const data = await res.json();
+      console.log(data);
+
+      updateUser(form);
+      setIsEditing(false);
+      alert("Perfil atualizado com sucesso!");
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async function handleSignOut() {
@@ -41,8 +70,6 @@ export default function Home() {
       navigate("/login");
     }
   }
-
-  console.log(user);
 
   return (
     <div style={{ padding: "20px", fontFamily: "sans-serif" }}>
@@ -67,7 +94,9 @@ export default function Home() {
                 style={{ padding: "5px" }}
                 type="text"
                 value={form.name}
-                onChange={(e) => setForm({ ...user, name: e.target.value })}
+                onChange={(e) =>
+                  setForm((prev) => ({ ...prev, name: e.target.value }))
+                }
               />
             </label>
             <label>
@@ -76,7 +105,9 @@ export default function Home() {
                 style={{ padding: "5px" }}
                 type="email"
                 value={form.email}
-                onChange={(e) => setForm({ ...user, email: e.target.value })}
+                onChange={(e) =>
+                  setForm((prev) => ({ ...prev, email: e.target.value }))
+                }
               />
             </label>
             <button type="submit">Salvar</button>
