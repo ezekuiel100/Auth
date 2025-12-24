9ce45385212e95fe469ec76f7404d16a26161193
unknown <ezekuiel100@gmail.com>
2025-12-24 17:23:33 -0300

    handle error on signupController

--- backend/src/controllers/signupController.ts
+++ backend/src/controllers/signupController.ts
@@ -1,22 +1,29 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 import { db } from "../database/index.js";
+import type { signupSchema } from "../schemas/index.js";
+import type { Static } from "@fastify/type-provider-typebox";
 
-export default function signupController(
-  request: FastifyRequest,
+const insertUser = db.prepare(`
+      INSERT INTO users (name, email, password)
+      VALUES (?,?,?)
+    `);
+
+type SignupBody = Static<typeof signupSchema>;
+
+export default async function signupController(
+  request: FastifyRequest<{ Body: SignupBody }>,
   reply: FastifyReply
 ) {
-  const { name, email, password } = request.body as {
-    name: string;
-    email: string;
-    password: string;
-  };
+  const { name, email, password } = request.body;
 
-  const insertUser = db.prepare(`
-        INSERT INTO users (name, email, password)
-        VALUES (?,?,?)
-      `);
+  try {
+    insertUser.run(name, email, password);
 
-  insertUser.run(name, email, password);
-
-  reply.send({ success: true });
+    reply.send({ success: true });
+  } catch (error: any) {
+    if (error.code === "SQLITE_CONSTRAINT_UNIQUE") {
+      return reply.status(409).send({ error: "E-mail já existe" });
+    }
+    return reply.status(500).send({ error: "Erro no banco" });
+  }
 }
