57dd6f66424dfe966de13a67cb80e6b6def0725b
unknown <ezekuiel100@gmail.com>
2025-12-25 09:45:16 -0300

    refactored signInController

--- backend/src/controllers/signinController.ts
+++ backend/src/controllers/signinController.ts
@@ -1,82 +1,19 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
-import { db } from "../database/index.js";
-import jwt from "jsonwebtoken";
 import { signinSchema } from "../schemas/index.js";
 import type { Static } from "@fastify/type-provider-typebox";
-import incrementFailedLoginAttempts from "../repositories/incrementFailedLoginAttempts.js";
-import bcrypt from "bcrypt";
-
-const secret = process.env.JWT_SECRET_KEY as string;
-
-if (!secret) {
-  throw new Error("A variável de ambiente JWT_SECRET_KEY não foi definida!");
-}
+import signInService from "../services/signInService.js";
+import { AuthError } from "../error.js";
 
 type SigninBody = Static<typeof signinSchema>;
 
-export default async function signinController(
+export default async function signInController(
   request: FastifyRequest<{ Body: SigninBody }>,
   reply: FastifyReply
 ) {
   try {
     const { email, password } = request.body;
 
-    const user = db.prepare("SELECT * FROM users WHERE email = ?").get(email);
-
-    if (!user) {
-      reply
-        .status(401)
-        .send({ success: false, message: "Credenciais erradas!" });
-      return;
-    }
-
-    const isLocked = user.lock_until && new Date(user.lock_until) > new Date();
-
-    if (isLocked) {
-      return reply.status(423).send({
-        success: false,
-        message: "Usuário temporariamente bloqueado",
-      });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      const { login_attempts } = incrementFailedLoginAttempts(email);
-
-      if (
-        login_attempts >= 5 &&
-        (!user.lock_until || new Date(user.lock_until) < new Date())
-      ) {
-        const lockUntilDate = new Date(
-          Date.now() + 15 * 60 * 1000
-        ).toISOString();
-
-        db.prepare(
-          "UPDATE users SET lock_until = ?, login_attempts = 0 WHERE email = ?"
-        ).run(lockUntilDate, email);
-
-        return reply.status(423).send({
-          success: false,
-          message: "Usuário temporariamente bloqueado",
-        });
-      }
-
-      reply
-        .status(401)
-        .send({ success: false, message: "Credenciais erradas!" });
-      return;
-    }
-
-    db.prepare(
-      "UPDATE users SET login_attempts = 0, lock_until = NULL WHERE email = ?"
-    ).run(email);
-
-    const token = jwt.sign({ id: user.id, email }, secret, {
-      expiresIn: "1h",
-    });
-
-    const { id, password: _, ...userWithoutPassword } = user;
+    const { user, token } = await signInService(email, password);
 
     reply
       .setCookie("token", token, {
@@ -86,13 +23,15 @@ export default async function signinController(
         sameSite: "lax",
         maxAge: 3600, // 1 hora em segundos
       })
-      .send({ success: true, user: userWithoutPassword });
+      .send(user);
   } catch (error) {
     request.log.error({ error, email: request.body.email });
 
-    reply.code(500).send({
-      success: false,
-      message: "Erro interno no servidor.",
-    });
+    if (error instanceof AuthError) {
+      reply.status(401).send({ message: error.message });
+      return;
+    }
+
+    reply.code(500).send({ message: "Erro interno no servidor." });
   }
 }
