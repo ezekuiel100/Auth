a469fcc1b85dc611ca0b98c6587db9a673fd375e
unknown <ezekuiel100@gmail.com>
2025-12-29 19:38:45 -0300

    remove try-catch from updateUserController

--- backend/src/controllers/updateUserController.ts
+++ backend/src/controllers/updateUserController.ts
@@ -8,27 +8,18 @@ export default function updateUserController(
   const user = request.user;
   const { name, email } = request.body as { name: string; email: string };
 
-  try {
-    const updateUser = db.prepare(
-      `UPDATE users 
+  const updateUser = db.prepare(
+    `UPDATE users 
       SET name = ?, email = ? 
       WHERE id = ? 
       `
-    );
+  );
 
-    const result = updateUser.run(name, email, user.id);
+  const result = updateUser.run(name, email, user.id);
 
-    if (result.changes === 0) {
-      return reply.status(404).send({ error: "Usuário não encontrado" });
-    }
-
-    reply.send({ success: true, message: "Dados atualizados com sucesso" });
-  } catch (error: unknown) {
-    if (error instanceof Error && (error as any).code === "SQLITE_CONSTRAINT") {
-      return reply.status(400).send({ error: "Este e-mail já está em uso" });
-    }
-
-    request.log.error(error);
-    return reply.status(500).send({ error: "Erro interno no servidor" });
+  if (result.changes === 0) {
+    return reply.status(404).send({ error: "Usuário não encontrado" });
   }
+
+  reply.send({ success: true, message: "Dados atualizados com sucesso" });
 }
