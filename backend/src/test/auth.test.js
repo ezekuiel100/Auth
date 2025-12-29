f4f51f2a80fec1049d7a6f370291c8a46fe6df9f
unknown <ezekuiel100@gmail.com>
2025-12-29 19:36:57 -0300

    test: add unauthorized scenario for invalid credentials

--- backend/src/test/auth.test.js
+++ backend/src/test/auth.test.js
@@ -1,9 +1,11 @@
 import assert from "node:assert"
 import { describe, it } from "node:test"
 
+const BASE_URL = "http://localhost:3000"
+
 describe("POST /auth/signin", () => {
     it("should signin with success", async () => {
-        const res = await fetch("http://localhost:3000/auth/signin", {
+        const res = await fetch(`${BASE_URL}/auth/signin`, {
             method: "POST",
             credentials: "include",
             headers: {
@@ -26,5 +28,29 @@ describe("POST /auth/signin", () => {
         const data = await res.json()
         assert.deepStrictEqual(data, { name: "test", email: "test@email.com" }, "Response body does not match expected user data")
     })
-})
 
+    it("should return 401 for invalid password", async () => {
+        const res = await fetch(`${BASE_URL}/auth/signin`, {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email: "test@email.com",
+                password: "1234xxxx"
+            })
+        })
+
+
+        assert.strictEqual(res.status, 401, "Response status should be 401")
+        assert.match(res.headers.get("content-type"), /application\/json/, "Response should be in JSON format");
+
+        const setCookie = res.headers.get("set-cookie");
+        assert.strictEqual(setCookie, null, "Should not send a cookie on failed login")
+
+        const data = await res.json()
+
+        assert.deepStrictEqual(data, { message: "Credenciais inválidas" })
+    })
+})
